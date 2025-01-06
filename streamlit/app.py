d02426393cb5bd24e2e1acf1f5b79651c46df135
DataValencio <datavalenco@hotmail.com>
2025-01-06 19:33:30 +0000

    nova pasta adicionada com codigo

---
+++ streamlit/app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+
+
+st.title('Criando meu primeiro site...')
+
+st.write('site criado com sucesso..')
+
+st.write('é só isso ?')
+
