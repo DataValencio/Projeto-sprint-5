5f5b5b6dd4570af776c4f928cc4e92bce58720df
DataValencio <datavalenco@hotmail.com>
2025-01-11 12:57:42 +0000

    new code fix

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ car_data = pd.read_csv('vehicles.csv')
 
 
 
-build_dispersao = st.checkbox('Criar um grafico de dispersão')
-if build_dispersao:
-            st.write('Criando um grafico de dispersão, por favor aguarde...')
-            fig1 = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
-            fig1.show()
+build_scatter_plot = st.checkbox("Criar gráfico de dispersão")
+
+if build_scatter_plot:
+    st.write(
+        "Criando um gráfico de dispersão para comparar a relação odomometro X preço do veiculo")
+
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True)
 
 
 hist_button = st.button('Criar histograma')
